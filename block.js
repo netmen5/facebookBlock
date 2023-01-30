bc40aace293e4415d8615ccd4d0c901bc2de951e
netmen5 <netmen5@gmail.com>
2023-01-30 11:23:58 +0100

    Create block.js

---
+++ block.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         FB main page hide
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  hide main page of FB
+// @author       netmen5
+// @match        *facebook.com
+// @exclude      *facebook.com/*
+// @grant        none
+// ==/UserScript==
